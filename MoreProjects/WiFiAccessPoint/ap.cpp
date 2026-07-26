b85bdec37eb7cf079cd48cf41d44e84e8ff34558
shiv32 <yadav.shivkumar5@gmail.com>
2026-07-26 16:43:21 +0530

    ap

--- MoreProjects/WiFiAccessPoint/ap.cpp
+++ MoreProjects/WiFiAccessPoint/ap.cpp
@@ -6,7 +6,7 @@
 #include <iostream>
 #include <string>
 
-bool execute(const std::string& cmd)
+bool execute(const std::string &cmd)
 {
     std::cout << "\n$ " << cmd << "\n\n";
     return std::system(cmd.c_str()) == 0;
@@ -14,8 +14,8 @@ bool execute(const std::string& cmd)
 
 void dashboard()
 {
-    std::system("clear");          // Clear terminal
-    
+    std::system("clear"); // Clear terminal
+
     std::cout << "\n========== Network Dashboard ==========\n";
     std::system("nmcli device status");
     std::cout << "------------------------------------------\n";
@@ -39,6 +39,8 @@ int main()
         std::cout << "\n1. Start Hotspot\n"
                   << "2. Stop Hotspot\n"
                   << "3. Refresh Dashboard\n"
+                  << "4. Unmanaged\n"
+                  << "5. Managed\n"
                   << "0. Exit\n"
                   << "Choice: ";
 
@@ -47,10 +49,11 @@ int main()
         switch (choice)
         {
         case 1:
-            execute("sudo iptables -P FORWARD ACCEPT");//Temporarily change the Linux forwarding policy to ACCEPT
+            execute("sudo iptables -P FORWARD ACCEPT"); // Temporarily change the Linux forwarding policy to ACCEPT
             execute(
                 "nmcli device wifi hotspot "
-                "ifname " + ifname +
+                "ifname " +
+                ifname +
                 " ssid AntHotspot "
                 "password yourPassword");
             dashboard();
@@ -64,7 +67,14 @@ int main()
         case 3:
             dashboard();
             break;
-
+        case 4:
+            std::system("sudo nmcli device set wlan0 managed no");
+            dashboard();
+            break;
+        case 5:
+            std::system("sudo nmcli device set wlan0 managed yes");
+            dashboard();
+            break;
         case 0:
             return 0;
 
